ddabb3649f32a09fafbc701d1e60c03594942071
andreeavochescu <elena.vochescu@ulbsibiu.ro>
2020-10-30 09:10:49 +0200

    initial commit

---
+++ src/main/java/com/park/parkinglot/resources/JavaEE8Resource.java
@@ -0,0 +1,20 @@
+package com.park.parkinglot.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("javaee8")
+public class JavaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
